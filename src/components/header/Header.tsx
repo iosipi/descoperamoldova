0a17c8b5a08b1a60656b25675584dae7443a8cb5
iosipi <iosipirina79@gmail.com>
2024-10-08 12:11:53 +0300

    acum

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Header() {
     return (
       <div className="Header">
-        
+        <ul>
+          <li>Centru</li>
+          <li>Nord</li>
+          <li>Sud</li>
+        </ul>
       </div>
     );
 }
